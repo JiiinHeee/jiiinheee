f297835adccc5217541091c32a8e064199287f61
jiiinheee <jiinii_wish@naver.com>
2022-10-14 21:51:18 +0900

    first commit

---
+++ src/shooting/EnemyAttack.java
@@ -0,0 +1,23 @@
+package shooting;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class EnemyAttack {
+	Image image = new ImageIcon("src/images/Enemy_attack.png").getImage();
+	int x, y;
+	int width = image.getWidth(null);
+	int height = image.getHeight(null);
+	int attack = 5;
+	
+	public EnemyAttack(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void fire() {
+		this.x -= 12;
+		
+	}
+
+}
---
+++ src/shooting/Main.java
@@ -0,0 +1,12 @@
+package shooting;
+
+public class Main {
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 840;
+	
+	public static void main(String[] args) {
+		new ShootingGame();
+		
+	}
+
+}
